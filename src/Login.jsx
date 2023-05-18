fa40260db9152f820e9610f2b721c149ec6ef1c5
Clement Felley <clement.fel@gmail.com>
2023-05-18 16:14:36 +0200

    Added Aave buttons

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,9 +3,18 @@ import SocialLogin from "@biconomy/web3-auth";
 import "@biconomy/web3-auth/dist/src/style.css"
 import { ChainId } from "@biconomy/core-types";
 import { ethers } from "ethers";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 export default function Login() {
-    const sdkRef = useRef();  
+    const sdkRef = useRef();
+    const [account, setAccount] = useState("")
+
+    const coinsData = [
+        { Asset: 'DAI', APY: '1%' },
+        { Asset: 'USDC', APY: '1%' },
+        { Asset: 'ETH', APY: '1%' }
+      ];
 
 
     useEffect(() => {
@@ -35,13 +44,28 @@ export default function Login() {
     };
 
     const showWallet = async e => {
+
         sdkRef.current?.showWallet()
+
+        if (!sdkRef.current?.provider) return;
+        const provider = new ethers.providers.Web3Provider(
+            sdkRef.current?.provider,
+        );
+        const accounts = await provider.listAccounts();
+        console.log(accounts)
+        setAccount(accounts)
+    }
+
+    const logout = async e => {
+        sdkRef.current?.logout()
+        console.log("Logged out")
+        setAccount('')
     }
     
 
     function web3Login() {
         return(
-            <div className="wallet mb-4 mt-4">
+            <div className="mb-4 mt-4">
                     <p className="text-center"><i> 
                         Login with your wallet provider or create a new wallet using your Gmail.
                     </i></p>
@@ -49,21 +73,125 @@ export default function Login() {
                     Connect web3auth
                 </button>
 
-            </div>          
+            </div>   
+
         )
     }
+
+    function web3logout() {
+        return (
+            <>
+            <div>
+                {account}
+            </div>
+            <div>
+            <button type="button" id="btn-login" className="btn btn-grey shadow-sm" onClick={logout}>
+                    Logout
+            </button>
+            </div>
+            </>
+        )
+
+    }
+
+
+    const DepositTemplate = (rowData) => {
+        let assetRow = rowData.Asset
+
+        switch (assetRow)
+
+        {
+           case "DAI":
+                return (
+                    <button type="button" className="btn rounded-pill btn-white shadow-sm" data-toggle="modal" data-target="#depositTableModal" style={{fontSize: "15px" }} >
+                        Deposit DAI
+                    </button>
+                )
+            
+            case "USDC":
+                return (
+                    <button type="button" className="btn rounded-pill btn-white shadow-sm" data-toggle="modal" data-target="#depositTableModal" style={{fontSize: "15px" }} >
+                        Deposit USDC
+                    </button>
+                )
+
+            case "ETH":
+                return (
+                    <button type="button" className="btn rounded-pill btn-white shadow-sm" data-toggle="modal" data-target="#depositTableModal" style={{fontSize: "15px" }} >
+                        Deposit ETH
+                    </button>
+                )
+                
+           default: 
+               console.log(assetRow);
+        }
+    }
+
+    const WithdrawTemplate = (rowData) => {
+        let assetRow = rowData.Asset
+
+        switch (assetRow)
+
+        {
+           case "DAI":
+                return (
+                    <button type="button" className="btn rounded-pill btn-white shadow-sm" data-toggle="modal" data-target="#depositTableModal" style={{fontSize: "15px" }} >
+                        Withdraw DAI
+                    </button>
+                )
+            
+            case "USDC":
+                return (
+                    <button type="button" className="btn rounded-pill btn-white shadow-sm" data-toggle="modal" data-target="#depositTableModal" style={{fontSize: "15px" }} >
+                        Withdraw USDC
+                    </button>
+                )
+
+            case "ETH":
+                return (
+                    <button type="button" className="btn rounded-pill btn-white shadow-sm" data-toggle="modal" data-target="#depositTableModal" style={{fontSize: "15px" }} >
+                        Withdraw ETH
+                    </button>
+                )
+                
+           default: 
+               console.log(assetRow);
+        }
+    }
+
 
 
     return (
         <>
-        <div className="background"> 
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col ">
-                        {web3Login()}                            
+        <div className="background">
+            {/*Login*/}
+            <section className="section">
+                <div className="center">
+                    {web3Login()}                            
+                </div>
+                <div className="mb-4 mt-4">
+                    <p className="text-center">
+                        {account? web3logout() : "Wallet not connected"}
+                    </p>
+                </div>
+            </section>
+
+            <hr />
+
+            {/*Aave*/}
+            <section className="section">
+                <div className="center">
+                    <div className="white">
+                    <DataTable value={coinsData} showGridlines responsiveLayout="scroll">
+                        <Column field="Asset" header="Asset" bodyClassName="text-center" style={{ width: '200px' }} alignHeader={'center'} ></Column>
+                        <Column field="Deposit" header="Deposit" body={DepositTemplate} bodyClassName="text-center" style={{ width: '200px' }} alignHeader={'center'} ></Column>
+                        <Column field="Withdraw" header="Withdraw" body={WithdrawTemplate} bodyClassName="text-center" style={{ width: '200px' }} alignHeader={'center'} ></Column>
+                    </DataTable>
                     </div>
                 </div>
-            </div>
+            </section>
+
+
         </div>
         </>
         )
