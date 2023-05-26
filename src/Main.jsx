1f6e328a905cb239227cf279aedce47f4bfe84f2
ChiElephant <anthony.merino@me.com>
2023-05-25 21:29:15 -0600

    testing rpcTarget

--- src/Main.jsx
+++ src/Main.jsx
@@ -137,7 +137,6 @@ export default function Main() {
             },
             network: network,
             rpcTarget: rpcTarget,
-
             })
             sdkRef.current = socialLoginSDK
         } catch (error) {
